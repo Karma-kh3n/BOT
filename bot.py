8b27c7e5057b83970af9938ddd919a1a91ca8190
Karma David <79895188+Karma-kh3n@users.noreply.github.com>
2022-06-09 15:30:43 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+#CODED BY KARMA DAVID
+#UPDATED 9 JUNE 2022
+
+
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(M))
+	exit()
+
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Nigeria" == fc:
+		__import__("bot").checkin()
+	else:
+		__import__("bot").checkin()
