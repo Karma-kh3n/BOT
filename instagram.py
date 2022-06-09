923303d297abb7c1e046f13dbc79da3d0bc0ceab
Karma David <79895188+Karma-kh3n@users.noreply.github.com>
2022-06-09 12:36:29 +0100

    Create instagram.py

---
+++ instagram.py
@@ -0,0 +1,18 @@
+#CODED BY KARMA DAVID
+#UPDATED 9 JUNE 2022
+
+= '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(M))
+	exit()
+
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Nigeria" == fc:
+		__import__("IG").checkin()
+	else:
+		__import__("IG").checkin()
